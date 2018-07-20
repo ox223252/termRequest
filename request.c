22d97a758c5d0076769fe73ebdc4f76438354567
ox223252 <23252@laposte.net>
2018-07-20 19:07:38 +0200

    First commit
    Changes to be committed:
    	new file:   request.c
    	new file:   request.h

---
+++ request.c
@@ -0,0 +1,107 @@
+
+////////////////////////////////////////////////////////////////////////////////
+/// \copiright ox223252, 2017
+///
+/// This program is free software: you can redistribute it and/or modify it
+///     under the terms of the GNU General Public License published by the Free
+///     Software Foundation, either version 2 of the License, or (at your
+///     option) any later version.
+///
+/// This program is distributed in the hope that it will be useful, but WITHOUT
+///     ANY WARRANTY; without even the implied of MERCHANTABILITY or FITNESS FOR
+///     A PARTICULAR PURPOSE. See the GNU General Public License for more
+///     details.
+///
+/// You should have received a copy of the GNU General Public License along with
+///     this program. If not, see <http://www.gnu.org/licenses/>
+////////////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+#include <termios.h>
+#include <unistd.h>
+#include <errno.h>
+
+int getPassword ( char * const password, const unsigned int size, const char byte )
+{
+	static struct termios oldMask, newMask;
+	unsigned int i  = 0;
+
+
+	if ( !password ||
+		!size )
+	{
+		errno = EINVAL;
+		return( __LINE__ );
+	}
+	
+	tcgetattr ( STDIN_FILENO, &oldMask );
+
+	newMask = oldMask;
+
+	newMask.c_lflag &= ~(ICANON); // avoid <enter>
+	newMask.c_lflag &= ~(ECHO); // hide text typed
+
+	tcsetattr( STDIN_FILENO, TCSANOW, &newMask );
+
+	while ( i < ( size - 1 ) )
+	{
+		password[ i ] = getchar ( );
+
+		if ( password[ i ] == '\n' )
+		{
+			password[ i ] = '\0';
+			break;
+		}
+		else if (  password[ i ] == 0x7f )
+		{ // manage backspace
+			if ( byte )
+			{
+				printf ( "\b  \b\b" );
+			}
+
+			password[ i ] = '\0';
+			if ( i )
+			{
+				i--;
+				password[ i ] = '\0';
+			}
+		}
+		else
+		{
+			if ( byte )
+			{
+				printf ( "%c", byte );
+			}
+			i++;
+		}
+	}
+
+	tcsetattr( STDIN_FILENO, TCSANOW, &oldMask);
+
+	return ( 0 );
+}
+
+#ifdef TEST_PASSWORD
+
+#include <string.h>
+
+int main ( void )
+{
+	char password[ 32 ] = { 0 };
+
+	printf ( "password : " );
+
+	getPassword ( password, 32, '*' );
+
+	printf ( "\ntyped : %s : %ld\n", password, strlen ( password ) );
+
+	printf ( "password : " );
+
+	getPassword ( password, 32, 0 );
+
+	printf ( "\ntyped : %s : %ld\n", password, strlen ( password ) );
+
+	return  ( 0 );
+}
+
+#endif
---
+++ request.h
@@ -0,0 +1,43 @@
+#ifndef __REQUEST_H__
+#define __REQUEST_H__
+////////////////////////////////////////////////////////////////////////////////
+/// \copiright ox223252, 2017
+///
+/// This program is free software: you can redistribute it and/or modify it
+///     under the terms of the GNU General Public License published by the Free
+///     Software Foundation, either version 2 of the License, or (at your
+///     option) any later version.
+///
+/// This program is distributed in the hope that it will be useful, but WITHOUT
+///     ANY WARRANTY; without even the implied of MERCHANTABILITY or FITNESS FOR
+///     A PARTICULAR PURPOSE. See the GNU General Public License for more
+///     details.
+///
+/// You should have received a copy of the GNU General Public License along with
+///     this program. If not, see <http://www.gnu.org/licenses/>
+////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
+/// \file request.h
+/// \brief lib manage termios request
+/// \author ox223252
+/// \date 2018-07
+/// \copyright GPLv2
+/// \version 0.1
+////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
+/// \fn int getPassword ( char * const password, const unsigned int size, 
+///     const char byte );
+/// \param [ in ] password : array for password output
+/// \param [ in ] size : array size
+/// \param [ in ] byte : byte to replace typed one could be set to 0 to hide 
+///     everything
+/// \brief request  to a password hide typed text and replace each letter by 
+///     byte if it was not zero
+/// \return if 0 the OK else see errno for more details
+////////////////////////////////////////////////////////////////////////////////
+int getPassword ( char * const password, const unsigned int size, 
+	const char byte );
+
+#endif
